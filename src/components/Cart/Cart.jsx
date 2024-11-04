2a280ef186ad719134c15b2008941fee3e5842db
Patricio Plem <patorafael.2000@gmail.com>
2024-11-04 04:49:49 -0300

    bueno empty funciona, aparecio un nuevo bug visual pero no debe ser muy dificil de solucionar

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 import cartService from '../../services/cartService';
 import ItemCard from './ItemCard';
 import ItemGrid from './ItemGrid.styles';
+import { Box } from '@mui/material';
 import { useCart } from '../../context/CartContext';
+import { Card } from '@mui/material';
 
 const Cart = () => {
   const { cart, setCart } = useCart();
@@ -12,18 +14,45 @@ const Cart = () => {
       setCart(data);
     });
   };
-  
+
+  const emptyCart = () => {
+    cartService.emptyCart().then(res => {
+      refreshCart();
+    });
+  };
+
+  const confirmCart = () => {
+    alert('Tanto no avance jajajaj');
+  };
+
   useEffect(() => {
     console.log(cart);
     refreshCart();
   }, []);
 
   return (
-    <ItemGrid>
-      {cart.items.map(item => (
-        <ItemCard key={item.id} item={item} />
-      ))}
-    </ItemGrid>
+    <Box>
+      <ItemGrid>
+        {cart.items.map(item => (
+          <ItemCard key={item.id} item={item} />
+        ))}
+      </ItemGrid>
+      <Card
+        style={{
+          display: 'flex',
+          minWidth: '50%',
+          alignContent: 'center',
+          alignSelf: 'center',
+          justifyContent: 'center',
+          justifyItems: 'center',
+          alignItems: 'center',
+          justifySelf: 'center'
+        }}>
+        <button onClick={emptyCart}>Empty Cart</button>
+        <h2 style={{ marginInline: '1px' }}>Total: ${cart.totalPrice}</h2>
+        <button onClick={confirmCart}>Confirm Cart</button>
+      </Card>
+    </Box>
   );
 };
 
--- src/services/cartService.js
+++ src/services/cartService.js
@@ -8,18 +8,20 @@ const cartService = {
 ,
 
   modifyItem: async itemData => {
-    console.log(itemData);
     const response = await authApi.put(`/cart/item`, itemData);
     return response.data.data;
   },
   
   removeItemFromCart: async product => {
 
-    console.log(product);
     const response = await authApi.put(`/cart/item/${product.id}`);
-    console.log(response.data.ok);
     return response.data.data;
-  }
+  },
+  emptyCart: async () => {
+
+    const response = await authApi.put(`/cart/empty`);
+    return response.data.data;
+  },
 }
 
 export default cartService;
