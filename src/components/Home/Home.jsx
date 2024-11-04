5831535d71a492f613c754624ff1db80f57d2353
Patricio Plem <patorafael.2000@gmail.com>
2024-11-04 15:56:51 -0300

    fixed it forced to loggin in Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,11 +3,12 @@ import { useEffect, useState } from 'react';
 import ProductGrid from '../Product/ProductGrid';
 import productService from '../../services/productService';
 import { useCart } from '../../context/CartContext';
+import { useUser } from '../../context/UserContext';
 import cartService from '../../services/cartService';
-
 const Home = () => {
   const [products, setProducts] = useState([]);
   const { cart, setCart } = useCart();
+  const { user } = useUser();
 
   const refreshProducts = () => {
     productService.getAllProducts().then(data => {
@@ -23,7 +24,10 @@ const Home = () => {
 
   useEffect(() => {
     refreshProducts();
-    refreshCart();
+    // si hago if (User) en el F5 no carga
+    if (sessionStorage.getItem('token')) {
+      refreshCart();
+    }
   }, []);
 
   return <ProductGrid products={products} />;
