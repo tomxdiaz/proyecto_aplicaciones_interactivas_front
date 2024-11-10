298820a0cc9d4bb4d0ba7aec4feca2165385767e
santiago <santiagonahuellopez@gmail.com>
2024-11-10 20:27:40 -0300

    Se agrega posibilidad de modificar la contraseña.

--- src/components/EditProfile/EditProfile.jsx
+++ src/components/EditProfile/EditProfile.jsx
@@ -1,4 +1,4 @@
-import { Button, Typography } from '@mui/material';
+import { Button, Divider, TextField, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import ROUTES, { getRoute } from '../../pages/routes';
@@ -13,8 +13,12 @@ import { accountType } from '../../utils/constants';
 import { useSnackbar } from '../../context/SnackbarContext';
 import userService from '../../services/userService';
 import { useUser } from '../../context/UserContext';
+import authService from '../../services/authenticateService';
 export const EditProfile = ({ user }) => {
   const [userData, setUserData] = useState(user);
+  const [pass, setPass] = useState('');
+  const [newPass1, setNewPass1] = useState('');
+  const [newPass2, setNewPass2] = useState('');
   const navigate = useNavigate();
   const { openSnackbar } = useSnackbar();
   const { setUser } = useUser();
@@ -49,18 +53,28 @@ export const EditProfile = ({ user }) => {
     setUserData(newUser);
   };
 
+  const updateData = () =>
+    userService
+      .updateUser(userData)
+      .then(newUserData => {
+        setUser(newUserData);
+        navigate(getRoute(ROUTES.PROFILE));
+      })
+      .catch(() => openSnackbar('Error al actualizar datos de la cuenta', 'error'));
+
   const handleSubmit = event => {
     event.preventDefault();
     try {
-      userService
-        .updateUser(userData)
-        .then(newUserData => {
-          setUser(newUserData);
-          navigate(getRoute(ROUTES.PROFILE));
-        })
-        .catch(() =>
-          openSnackbar('Error al actualizar datos de la cuenta', 'error')
-        );
+      if (newPass1 || newPass2) {
+        if (newPass1 !== newPass2)
+          openSnackbar('Las contraseñas deben coincidir', 'error');
+        else if (!pass) openSnackbar('Debe ingresar la contraseña actual', 'error');
+        else
+          authService
+            .login(userData.username, pass)
+            .then(() => updateData())
+            .catch(() => openSnackbar('La contraseña es incorrecta', 'error'));
+      } else updateData();
     } catch (error) {
       openSnackbar('Error al actualizar datos de la cuenta', 'error');
     }
@@ -71,24 +85,51 @@ export const EditProfile = ({ user }) => {
       <Typography variant='h4'>Actualizar información</Typography>
       <ProfileForm onSubmit={handleSubmit}>
         <Avatar />
-        {userInputs.map(input => (
-          <Input
-            label={input.label}
-            state={input.state}
-            value={input.value}
-            type={input.type}
-            handleChange={handleChange}
-            disabled={input.disabled}
-            key={`editProfile-input-${input.label}`}
-          />
-        ))}
+        <>
+          {userInputs.map(input => (
+            <Input
+              label={input.label}
+              state={input.state}
+              value={input.value}
+              type={input.type}
+              handleChange={handleChange}
+              disabled={input.disabled}
+              key={`editProfile-input-${input.label}`}
+            />
+          ))}
+        </>
+        <Divider orientation='horizontal' flexItem sx={{ margin: '1rem 0' }} />
+        <Typography variant='h5'>Cambiar contraseña</Typography>
+        <TextField
+          label='Contraseña actual'
+          value={pass}
+          fullWidth
+          margin='normal'
+          onChange={event => setPass(event.target.value)}
+          autoComplete='new-password'
+        />
+        <TextField
+          label='Nueva contraseña'
+          value={newPass1}
+          fullWidth
+          margin='normal'
+          onChange={event => setNewPass1(event.target.value)}
+          autoComplete='new-password'
+        />
+        <TextField
+          label='Confirme nueva contraseña'
+          value={newPass2}
+          fullWidth
+          margin='normal'
+          onChange={event => setNewPass2(event.target.value)}
+          autoComplete='new-password'
+        />
         <FormActions>
           <Link to={getRoute(ROUTES.PROFILE)}>
             <Button type='submit' variant='contained' color='primary'>
               Cancelar
             </Button>
           </Link>
-
           <Button type='submit' variant='contained' color='primary'>
             Guardar
           </Button>
